2d2f567cb1a5d18da6ab07029d2b9ef5957cc58b
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-12-19 10:04:21 +0700

    fix bug in network architecure

--- Segmentation/pavement_crack_detection_pytorch/fphn/network.py
+++ Segmentation/pavement_crack_detection_pytorch/fphn/network.py
@@ -55,24 +55,78 @@ class ConcatLayer(nn.Module):
         super().__init__()
         self.upsample_map = nn.Upsample(scale_factor=2, mode='bicubic')
         self.padding = nn.ReflectionPad2d((0, 1, 0, 1))
+        self.reduce_depth = nn.Conv2d(768, 128, kernel_size = (1,1), stride = 1, padding = 1)
     def forward(self, input_layer, up_input_layer):
         size_input = list(input_layer.size())
-        upsample_map = nn.Upsample((size_input[2], size_input[3]), mode='bicubic')(up_input_layer)
-        size_upsample = list(upsample_map.size())
+        upsample_map = self.upsample_map(up_input_layer)
         concat = torch.cat([input_layer, upsample_map], 1)
-        reduce_depth =  nn.Conv2d(size_input[1] +size_upsample[1], 128, kernel_size = (1,1), stride = 1, padding = 1)(concat)
-        # norm = nn.InstanceNorm2d(num_features = 128)(reduce_depth)
-        # output = nn.ReLU(inplace = True)(norm)
+        output =  self.reduce_depth(concat)
         return output
             
+class ConcatLayer256(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.upsample_map = nn.Upsample(scale_factor=2, mode='bicubic')
+        self.padding = nn.ReflectionPad2d((0, 1, 0, 1))
+        self.reduce_depth = nn.Conv2d(256, 128, kernel_size = (1,1), stride = 1, padding = 0)
+    def forward(self, input_layer, up_input_layer):
+        size_input = list(input_layer.size())
+        size_up_input = list(up_input_layer.size())
+        if size_up_input[2] < size_input[2]:
+            upsample_map = self.upsample_map(up_input_layer)
+            concat = torch.cat([input_layer, upsample_map], 1)
+            output =  self.reduce_depth(concat)
+            return output
+        else:
+            concat = torch.cat([input_layer, up_input_layer], 1)
+            output =  self.reduce_depth(concat)
+            return output
+
+class ConcatLayer192(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.upsample_map = nn.Upsample(scale_factor=2, mode='bicubic')
+        self.padding = nn.ReflectionPad2d((0, 1, 0, 1))
+        self.reduce_depth = nn.Conv2d(192, 128, kernel_size = (1,1), stride = 1, padding = 0)
+    def forward(self, input_layer, up_input_layer):
+        size_input = list(input_layer.size())
+        size_up_input = list(up_input_layer.size())
+        if size_up_input[2] < size_input[2]:
+            upsample_map = self.upsample_map(up_input_layer)
+            concat = torch.cat([input_layer, upsample_map], 1)
+            output =  self.reduce_depth(concat)
+            return output
+        else:
+            concat = torch.cat([input_layer, up_input_layer], 1)
+            output =  self.reduce_depth(concat)
+            return output
+
+class ConcatLayer23(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.upsample_map = nn.Upsample(scale_factor=2, mode='bicubic')
+        self.padding = nn.ReflectionPad2d((0, 1, 0, 1))
+        self.reduce_depth = nn.Conv2d(256, 128, kernel_size = (1,1), stride = 1, padding = 0)
+    def forward(self, input_layer, up_input_layer):
+        size_input = list(input_layer.size())
+        size_up_input = list(up_input_layer.size())
+        upsample_map = self.upsample_map(up_input_layer)
+        concat = torch.cat([input_layer, upsample_map], 1)
+        output =  self.reduce_depth(concat)
+        return output
+
 class SideNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv11 = nn.Conv2d(512, 512, kernel_size=(1,1), stride=1, padding=1)
-        self.deconv = nn.ConvTranspose2d(512, 512, kernel_size=(3,3), stride=1, padding=1)
+        self.conv11 = nn.Conv2d(128, 128, kernel_size=(1,1), stride=1, padding=1)
+        self.deconv = nn.ConvTranspose2d(128, 128, kernel_size=(3,3), stride=1, padding=1)
     def forward(self, x):
         x = self.conv11(x)
         x = self.deconv(x)
+        size_x = list(x.size())
+        while size_x[2] < 128:
+            x = self.deconv(x)
+            size_x = list(x.size())
         x = nn.ReLU(inplace = True)(x)
         return x
 
@@ -89,16 +143,19 @@ class FphbNet(nn.Module):
             nn.ReLU(inplace = True)
         )
         self.size_network = SideNetwork()
-        self.concat_layer = ConcatLayer()
+        self.concat_layer256 = ConcatLayer256()
+        self.concat_layer192 = ConcatLayer192()
+        self.concat_layer23 = ConcatLayer23()
+
 
     def forward(self, x):
         feature_map_layer_1 = self.bottom_up_layer_1(x)
         feature_map_layer_2 = self.bottom_up_layer_2(feature_map_layer_1)
         feature_map_layer_3 = self.bottom_up_layer_3(feature_map_layer_2)
 
-        feature_pyramid_3 = self.concat_layer(feature_map_layer_2, feature_map_layer_3)
-        feature_pyramid_2 = self.concat_layer(feature_map_layer_2, feature_pyramid_3)
-        feature_pyramid_1 = self.concat_layer(feature_map_layer_1, feature_pyramid_2)
+        feature_pyramid_3 = self.concat_layer23(feature_map_layer_2, feature_map_layer_3)
+        feature_pyramid_2 = self.concat_layer256(feature_map_layer_2, feature_pyramid_3)
+        feature_pyramid_1 = self.concat_layer192(feature_map_layer_1, feature_pyramid_2)
 
         side_network_3 = self.size_network(feature_map_layer_3)
         side_network_2 = self.size_network(feature_pyramid_2)
