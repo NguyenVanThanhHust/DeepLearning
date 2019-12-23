d07c5545ddae345eeb25de24ace63b7806d64a2e
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-12-23 14:08:07 +0700

    remove object detection

--- ObjectDetection/custom_dataset.py
+++ ObjectDetection/custom_dataset.py
@@ -40,24 +40,28 @@ class CustomDataset:
         content = [x.rstrip('\n') for x in content]
         bbox = list()
         label = list() 
+        scale = list()
         for eachline in content:
             list_number = re.findall(r'\d+', str(eachline))
-            x1, y1, x2, y2, number = list_number
             if len(list_number) > 0:
+                x1, y1, x2, y2, number = list_number
                 bbox.append([y1, x1, y2, x2])
                 label.append(number)
+                scale.append(1)
             else:
                 print("filepath: ", file_path, " list number: ", list_number)
 
         bbox = np.stack(bbox).astype(np.float32)
         label = np.stack(label).astype(np.int32)
+        scale = np.stack(scale).astype(np.int32)
+
         # Load a image
         img_file = os.path.join(self.data_dir, 'positive image set', id_ + '.jpg')
         img = read_image(img_file, color=True)
 
         # if self.return_difficult:
         #     return img, bbox, label, difficult
-        return img, bbox, label
+        return img, bbox, label, scale
 
     __getitem__ = get_example
 
